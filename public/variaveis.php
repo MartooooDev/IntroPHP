9770600a7ba99fd24df04132876e805a25607845
MartooooDev <martoooodev@gmail.com>
2024-04-06 23:23:58 -0300

    Updating all the files

---
+++ public/variaveis.php
@@ -0,0 +1,22 @@
+<?php
+    //String
+    $nome               =   "Martin";
+
+    //Number
+    //Integer
+    $numeroInteiro      =   12;
+    //Double/ Float
+    $numeroFracionado   =   3.14;
+    
+    //Boolean
+    $mundoExplodiu      =   False;
+    $vouAprenderPHP     =   True;
+
+    //Arrays
+
+    $array              =   [1, 2, 3,];
+    //Object
+
+    //Null
+
+?>
